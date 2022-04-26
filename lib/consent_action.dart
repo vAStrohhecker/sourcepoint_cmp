ac15f5f0d674b9c08950a3118bcb9ac143d9e3dd
Jan Strohhecker <jan.strohhecker@vonaffenfels.de>
2022-04-26 09:36:50 +0200

    Updated to CMP Lib 6.4.2

---
+++ lib/consent_action.dart
@@ -0,0 +1,9 @@
+import 'package:sourcepoint_cmp/action_type.dart';
+
+class ConsentAction {
+  final ActionType? actionType;
+  final String? customActionId;
+
+  ConsentAction(this.actionType, {this.customActionId});
+
+}
